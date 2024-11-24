dc167531c102852b924d4c205dbccf1bca648963
Indraja lakshmi Tirumalanadhuni <88099897+indrajalakshmi2001@users.noreply.github.com>
2024-11-24 16:42:34 +0530

    Delete djangoProjects directory

--- djangoProjects/calc/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'home.html',{'name': 'sai'})
-def add(request):
-    val1 = int(request.POST["num1"])
-    val2 = int(request.POST["num2"])
-    res = val1 + val2
-    return render(request,"result.html",{'result':res})
